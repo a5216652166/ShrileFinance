88d43d25947a6cac0016a0ce0ccbc7b95c1baa8e
严冬 <753045147@qq.com>
2016-12-16 17:23:01 +0800

    完成机构和关注实体创建

---
+++ Core/Entities/Message/BorrowMessage/Concern/ConcernBase.cs
@@ -0,0 +1,41 @@
+﻿namespace Core.Entities.Message.BorrowMessage.Concern
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ConcernBase
+    {
+        [StringLength(4), MinLength(4), Required]
+        public string 信息记录长度 { get; set; }
+
+        [StringLength(2), MinLength(2), Required]
+        public string 信息记录类型 { get; set; }
+
+        public string 信息类别
+        {
+            get { return "B"; }
+        }
+
+        [StringLength(11), Required, MinLength(11)]
+        public string 金融机构代码
+        {
+            get { return "33207991216"; }
+        }
+
+        [StringLength(80), Required]
+        public string 借款人名称 { get; set; }
+
+        [StringLength(16), MinLength(16), Required]
+        public string 贷款卡编码 { get; set; }
+
+        [StringLength(1), Required]
+        public string 信息记录操作类型 { get; set; }
+
+        [StringLength(8), Required]
+        public string 业务发生日期 { get; set; }
+
+        public string 信息记录跟踪编号
+        {
+            get { return string.Empty.PadLeft(20, '0'); }
+        }
+    }
+}
---
+++ Core/Entities/Message/BorrowMessage/Concern/Litigation.cs
@@ -0,0 +1,53 @@
+﻿namespace Core.Entities.Message.BorrowMessage.Concern
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Litigation
+    {
+        public string 信息类别
+        {
+            get { return "B"; }
+        }
+
+        /// <summary>
+        /// 被起诉流水号
+        /// </summary>
+        [Required]
+        public string ChargedSerialNumber { get; set; }
+
+        /// <summary>
+        /// 起诉人姓名
+        /// </summary>
+        [Required]
+        public string ProsecuteName { get; set; }
+
+        public string 币种
+        {
+            get { return "CNY"; }
+        }
+
+        /// <summary>
+        /// 判决执行金额
+        /// </summary>
+        [Display(Name = "判决执行金额"), Required]
+        public decimal? Money { get; set; }
+
+        /// <summary>
+        /// 判决执行日期
+        /// </summary>
+        [Required]
+        public string DateTime { get; set; }
+
+        /// <summary>
+        /// 执行结果
+        /// </summary>
+        [Required]
+        public string Result { get; set; }
+
+        /// <summary>
+        /// 被起诉原因
+        /// </summary>
+        [Required]
+        public string Reason { get; set; }
+    }
+}
