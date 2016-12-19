7b9d3f0e506970851b38f1b6727068251764afe2
kurumi <kurumi@KURUMI-PC>
2016-12-19 16:40:26 +0800

    派生类

---
+++ Core/Entities/CreditInvestigation/Datagram/Borrower/BorrowerCapitalStructureDatagram.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Entities.CreditInvestigation.Datagram.Borrower
+{
+    using System;
+
+    /// <summary>
+    /// 借款人资本构成信息报文
+    /// </summary>
+    public class BorrowerCapitalStructureDatagram : AbsDatagram
+    {
+        public BorrowerCapitalStructureDatagram()
+        {
+        }
+
+        public override byte Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
