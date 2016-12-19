185e4abaaadff552c780a94c3ba14b25e7b344ce
严冬 <753045147@qq.com>
2016-12-19 14:31:56 +0800

    添加担保基础实体

---
+++ Core/Entities/CreditInvestigation/Segment/CreditMessage/Guarantee.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities.CreditInvestigation.Segment.CreditMessage
+{
+    public class Guarantee
+    {
+        public string 信息类别
+        {
+            get { return "D"; }
+        }
+    }
+}
---
+++ Core/Entities/CreditInvestigation/Segment/CreditMessage/GuaranteeBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.CreditInvestigation.Segment.CreditMessage
+{
+    public class GuaranteeBase
+    {
+        public string 信息记录长度 { get; set; }
+
+        public string 信息记录类型 { get; set; }
+
+        public string 信息类别
+        {
+            get { return "B"; }
+        }
+
+        public string 金融机构代码 { get; set; }
+
+        /// <summary>
+        /// 中征码/贷款卡编码
+        /// </summary>
+        public string LoanCardCode { get; set; }
+
+        /// <summary>
+        /// 主合同编码
+        /// </summary>
+        public string CreditId { get; set; }
+
+        public string 信贷业务种类
+        {
+            get { return "1"; }
+        }
+
+        public string 信息记录操作类型
+        {
+            get { return "1"; }
+        }
+
+        public string 业务发生日期
+        {
+            get { return DateTime.Now.ToString("yyyyMMdd"); }
+        }
+
+        public string 信息记录跟踪编号
+        {
+            get{ return string.Empty.PadLeft(20, '0');}
+        }
+    }
+}
