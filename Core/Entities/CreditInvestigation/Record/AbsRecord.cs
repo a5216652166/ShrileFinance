63017b6bdaefda769e4e8768b3659bfe37ce779c
七月 <foolishqi@126.com>
2016-12-19 14:21:34 +0800

    抽象类定义

---
+++ Core/Entities/CreditInvestigation/Record/AbsRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities.CreditInvestigation.Record
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class AbsRecord : Entity
+    {
+    }
+}
