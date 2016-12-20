88d21f35f71dde5eb64ed60ad19baf64c8569187
kurumi <kurumi@KURUMI-PC>
2016-12-20 11:38:13 +0800

    机构财务大事件信息记录

---
+++ Core/Entities/CreditInvestigation/Record/OrganizationRecords/ConcernInfo/BigEventRecord.cs
@@ -0,0 +1,26 @@
+﻿namespace Core.Entities.CreditInvestigation.Record.OrganizationRecords.ConcernInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using Segment;
+
+    /// <summary>
+    /// 大事件信息记录
+    /// </summary>
+    public class BigEventRecord : AbsRecord
+    {
+        public BigEventRecord()
+        {
+        }
+
+        public override RecordTypeEnum Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override ICollection<AbsSegment> Segments { get; protected set; }
+    }
+}
---
+++ Core/Entities/CreditInvestigation/Record/OrganizationRecords/FinancialAffiarInfo/BalanceSheetRecord.cs
@@ -0,0 +1,26 @@
+﻿namespace Core.Entities.CreditInvestigation.Record.OrganizationRecords.FinancialAffiarInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using Segment;
+
+    /// <summary>
+    /// 2007版资产负债表信息记录
+    /// </summary>
+    public class BalanceSheetRecord : AbsRecord
+    {
+        public BalanceSheetRecord()
+        {
+        }
+
+        public override RecordTypeEnum Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override ICollection<AbsSegment> Segments { get; protected set; }
+    }
+}
