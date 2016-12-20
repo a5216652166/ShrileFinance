c677ab2e2d3711e78c926dd54cb5163da383ce2a
王鹏飞 <380807789@qq.com>
2016-12-20 14:29:57 +0800

    5大信息记录的注解属性配置

--- Core/Entities/CreditInvestigation/Segment/BorrowMessage/FinancialAffair/CashFlowParagraph.cs
+++ Core/Entities/CreditInvestigation/Segment/BorrowMessage/FinancialAffair/CashFlowParagraph.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffair
+﻿namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffair
 {
     /// <summary>
     /// 2007版现金流量表信息记录
@@ -14,6 +8,7 @@ namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffai
         /// <summary>
         /// 信息类别
         /// </summary>
+        [MetaCode(1, MetaCodeTypeEnum.AN), SegmentRule(1, true)]
         public string Type
         {
             get { return "I"; }
@@ -22,316 +17,379 @@ namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffai
         /// <summary>
         /// 销售商品和提供劳务收到的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(2, false)]
         public decimal? SaleGoodsCash { get; set; }
 
         /// <summary>
         /// 收到的税费返还
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(22, false)]
         public decimal? TaxesRefunds { get; set; }
 
         /// <summary>
         /// 收到其他与经营活动有关的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(42, false)]
         public decimal? ReceiveOperatingActivitiesCash { get; set; }
 
         /// <summary>
         /// 经营活动现金流入小计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(62, false)]
         public decimal? OperatingActivitiesCashInflows { get; set; }
 
         /// <summary>
         /// 购买商品、接受劳务支付的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(82, false)]
         public decimal? BuyGoodsCash { get; set; }
 
         /// <summary>
         /// 支付给职工以及为职工支付的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(102, false)]
         public decimal? PayEmployeeCash { get; set; }
 
         /// <summary>
         /// 支付的各项税费
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(122, false)]
         public decimal? PayAllTaxes { get; set; }
 
         /// <summary>
         /// 支付其他与经营活动有关的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(142, false)]
         public decimal? PayOperatingActivitiesCash { get; set; }
 
         /// <summary>
         /// 经营活动现金流出小计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(162, false)]
         public decimal? OperatingActivitiesCashOutflows { get; set; }
 
         /// <summary>
         /// 经营活动产生的现金流量净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(182, true)]
         public decimal OperatingActivitieCashNet { get; set; }
 
         /// <summary>
         /// 收回投资所收到的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(202, false)]
         public decimal? RecoveryInvestmentCash { get; set; }
 
         /// <summary>
         /// 取得投资收益所收到的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(222, false)]
         public decimal? InvestmentIncomeCash { get; set; }
 
         /// <summary>
         /// 处置固定资产无形资产和其他长期资产所收回的现金净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(242, false)]
         public decimal? RecoveryAssetsCash { get; set; }
 
         /// <summary>
         /// 处置子公司及其他营业单位收到的现金净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(262, false)]
         public decimal? RecoveryChildrenCompanyCash { get; set; }
 
         /// <summary>
         /// 收到其他与投资活动有关的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(282, false)]
         public decimal? OtherInvestmentCash { get; set; }
 
         /// <summary>
         /// 投资活动现金流入小计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(302, false)]
         public decimal? CashInInvestmentActivities { get; set; }
 
         /// <summary>
         /// 购建固定资产无形资产和其他长期资产所支付的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(322, false)]
         public decimal? BuyAssetsCash { get; set; }
 
         /// <summary>
         /// 投资所支付的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(342, false)]
         public decimal? InvestmentPaidCash { get; set; }
 
         /// <summary>
         /// 取得子公司及其他营业单位支付的现金净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(362, false)]
         public decimal? GetChildrenComponyCash { get; set; }
 
         /// <summary>
         /// 支付其他与投资活动有关的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(382, false)]
         public decimal? PayOtherInvestmentCash { get; set; }
 
         /// <summary>
         /// 投资活动现金流出小计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(402, false)]
         public decimal? InvestmentCashOutflow { get; set; }
 
         /// <summary>
         /// 投资活动产生的现金流量净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(422, true)]
         public decimal InvestmentCashInflow { get; set; }
 
         /// <summary>
         /// 吸收投资收到的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(442, false)]
         public decimal? AbsorbInvestmentCash { get; set; }
 
         /// <summary>
         /// 取得借款收到的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(462, false)]
         public decimal? GetLoanCash { get; set; }
 
         /// <summary>
         /// 收到其他与筹资活动有关的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(482, false)]
         public decimal? GetFinancingCash { get; set; }
 
         /// <summary>
         /// 筹资活动现金流入小计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(502, false)]
         public decimal? FinancingCashInflow { get; set; }
 
         /// <summary>
         /// 偿还债务所支付的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(522, false)]
         public decimal? DebtRedemption { get; set; }
 
         /// <summary>
         /// 分配股利、利润或偿付利息所支付的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(542, false)]
         public decimal? PayCashForDividend { get; set; }
 
         /// <summary>
         /// 其他
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(562, false)]
         public decimal? PayFinancingCash { get; set; }
 
         /// <summary>
         /// 支付其他与筹资活动有关的现金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(582, false)]
         public decimal? PayOtherFinancingCash { get; set; }
 
         /// <summary>
         /// 筹资活动现金流出小计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(602, true)]
         public decimal? FinancingCashOutflow { get; set; }
 
         /// <summary>
         /// 筹集活动产生的现金流量净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(622, false)]
         public decimal FinancingNetCash { get; set; }
 
         /// <summary>
         /// 汇率变动对现金及现金等价物的影响
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(642, true)]
         public decimal? ExchangeRateChangeCash { get; set; }
 
         /// <summary>
         /// 现金及现金等价物净增加额(五)
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(662, false)]
         public decimal CashIncrease5 { get; set; }
 
         /// <summary>
         /// 期初现金及现金等价物余额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(682, true)]
         public decimal? BeginCashBalance { get; set; }
 
         /// <summary>
         /// 期末现金及现金等价物余额(六)
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(702, false)]
         public decimal FinalCashBalance6 { get; set; }
 
         /// <summary>
         /// 净利润
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(722, false)]
         public decimal? NetProfit { get; set; }
 
         /// <summary>
         /// 资产减值准备
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(742, false)]
         public decimal? AssetImpairment { get; set; }
 
         /// <summary>
         /// 固定资产折旧、油气资 产折耗、生产性生物资产折旧
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(762, false)]
         public decimal? AssetsDepreciation { get; set; }
 
         /// <summary>
         /// 无形资产摊销
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(782, false)]
         public decimal? IntangibleAssetsAmortization { get; set; }
 
         /// <summary>
         /// 长期待摊费用摊销
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(802, false)]
         public decimal? LongPrepaidExpenses { get; set; }
 
         /// <summary>
         /// 待摊费用减少
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(822, false)]
         public decimal? PrepaidExpensesLessen { get; set; }
 
         /// <summary>
         /// 预提费用增
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(842, false)]
         public decimal? AccruedExpenses { get; set; }
 
         /// <summary>
         /// 处置固定资产无形资产和其他长期资产的损失
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(862, false)]
         public decimal? Assetloss { get; set; }
 
         /// <summary>
         /// 固定资产报废损失
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(882, false)]
         public decimal? FixedAssetsScrap { get; set; }
 
         /// <summary>
         /// 公允价值变动损失
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(902, false)]
         public decimal? FairChanges { get; set; }
 
         /// <summary>
         /// 财务费用
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(922, false)]
         public decimal? FinancialExpenses { get; set; }
 
         /// <summary>
         /// 投资损失
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(942, false)]
         public decimal? InvestmentLosses { get; set; }
 
         /// <summary>
         /// 递延所得税资产减少
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(962, false)]
         public decimal? DeferredIncomeTaxLessen { get; set; }
 
         /// <summary>
         /// 递延所得税资产增加
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(982, false)]
         public decimal? DeferredIncomeTaAdd { get; set; }
 
         /// <summary>
         /// 存货的减少
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1002, false)]
         public decimal? Inventoryreduction { get; set; }
 
         /// <summary>
         /// 经营性应收项目的减少
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1022, false)]
         public decimal? ReceivableItemLosses { get; set; }
 
         /// <summary>
         /// 经营性应付项目的增加
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1042, false)]
         public decimal? PayablesAdd { get; set; }
 
         /// <summary>
         /// 其他
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1062, false)]
         public decimal? Other { get; set; }
 
         /// <summary>
         /// 经营活动产生的现金流量净额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1082, false)]
         public decimal? OperatingCashFlowsNet { get; set; }
 
         /// <summary>
         /// 债务转为资本
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1102, false)]
         public decimal? CapitalDebt { get; set; }
 
         /// <summary>
         /// 一年内到期的可转换公司债券
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1122, false)]
         public decimal? CorporateBondInYear { get; set; }
 
         /// <summary>
         /// 融资租入固定资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1142, false)]
         public decimal? FinancingFixedAssets { get; set; }
 
         /// <summary>
         /// 现金的期末余额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1162, false)]
         public decimal? EndingBalance { get; set; }
 
         /// <summary>
         /// 现金的期初余额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1182, false)]
         public decimal? BeginBalance { get; set; }
 
         /// <summary>
         /// 现金等价物的期末余额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1202, false)]
         public decimal? CashEquivalentsEndingBalance { get; set; }
 
         /// <summary>
         /// 现金等价物的期初余额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1222, false)]
         public decimal? CashEquivalentsBeginBalance { get; set; }
 
         /// <summary>
         /// 现金及现金等价物净增加额
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1242, false)]
         public decimal? CashEquivalentsNetIncrease { get; set; }
     }
 }
--- Core/Entities/CreditInvestigation/Segment/BorrowMessage/FinancialAffair/LiabilitiesParagraph.cs
+++ Core/Entities/CreditInvestigation/Segment/BorrowMessage/FinancialAffair/LiabilitiesParagraph.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffair
+﻿namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffair
 {
     /// <summary>
     /// 2007版资产负债表信息记录
@@ -14,6 +8,7 @@ namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffai
         /// <summary>
         /// 信息类别
         /// </summary>
+        [MetaCode(1, MetaCodeTypeEnum.AN), SegmentRule(1, true)]
         public string Type
         {
             get { return "G"; }
@@ -22,302 +17,361 @@ namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffai
         /// <summary>
         /// 货币资金
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(2, false)]
         public decimal? MonetaryFund { get; set; }
 
         /// <summary>
         /// 交易性金融资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(22, false)]
         public decimal? TransactionAssets { get; set; }
 
         /// <summary>
         /// 应收票据
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(42, false)]
         public decimal? NoteReceivable { get; set; }
 
         /// <summary>
         /// 应收账款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(62, false)]
         public decimal? AccountsReceivable { get; set; }
 
         /// <summary>
         /// 预付账款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(82, false)]
         public decimal? AdvancePayment { get; set; }
 
         /// <summary>
         /// 应收利息
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(102, false)]
         public decimal? InterestReceivable { get; set; }
 
         /// <summary>
         /// 应收股利
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(122, false)]
         public decimal? DividendsReceivable { get; set; }
 
         /// <summary>
         /// 其他应收款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(142, false)]
         public decimal? OtherReceivables { get; set; }
 
         /// <summary>
         /// 存货
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(162, false)]
         public decimal? Inventory { get; set; }
 
         /// <summary>
         /// 一年内到期的非流动资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(182, false)]
         public decimal? NonCurrentAssetsInYear { get; set; }
 
         /// <summary>
         /// 其他流动资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(202, false)]
         public decimal? OtherCurrentAssets { get; set; }
 
         /// <summary>
         /// 流动资产合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(222, true)]
         public decimal TotalCurrentAssets { get; set; }
 
         /// <summary>
         /// 可供出售的金融资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(242, false)]
         public decimal? CanSaleAsset { get; set; }
-
 
         /// <summary>
         /// 持有至到期的投资
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(262, false)]
         public decimal? MaturityInvestment { get; set; }
 
         /// <summary>
         /// 长期股权投资
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(282, false)]
         public decimal? LongEquity { get; set; }
 
         /// <summary>
         /// 长期应收款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(302, false)]
         public decimal? LongReceivables { get; set; }
 
         /// <summary>
         /// 投资性房地产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(322, false)]
         public decimal? InvestmentRealEstate { get; set; }
 
         /// <summary>
         /// 固定资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(342, false)]
         public decimal? FixedAssets { get; set; }
 
         /// <summary>
         /// 在建工程
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(362, false)]
         public decimal? ConstructionProject { get; set; }
 
         /// <summary>
         /// 工程物资
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(382, false)]
         public decimal? EngineeringMaterials { get; set; }
 
         /// <summary>
         /// 固定资产清理
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(402, false)]
         public decimal? FixedAssetsLiquidation { get; set; }
 
         /// <summary>
         /// 生产性生物资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(422, false)]
         public decimal? ProductiveBiologicalAssets { get; set; }
 
         /// <summary>
         /// 油气资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(442, false)]
         public decimal? OilGasAssets { get; set; }
 
         /// <summary>
         /// 无形资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(462, false)]
         public decimal? IntangibleAssets { get; set; }
 
         /// <summary>
         /// 开发支出
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(482, false)]
         public decimal? DevelopmentExpenditure { get; set; }
 
         /// <summary>
         /// 商誉
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(502, false)]
         public decimal? Goodwill { get; set; }
 
         /// <summary>
         /// 长期待摊费用
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(522, false)]
         public decimal? LongArepaidExpenses { get; set; }
 
         /// <summary>
         /// 递延所得税资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(542, false)]
         public decimal? DeferredTaxAssets { get; set; }
 
         /// <summary>
         /// 其他非流动资产
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(562, false)]
         public decimal? OtherNonCurrentAssets { get; set; }
 
         /// <summary>
         /// 非流动资产合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(582, true)]
         public decimal TotalNonCurrentAssets { get; set; }
 
         /// <summary>
         /// 资产总计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(602, true)]
         public decimal TotalAssets { get; set; }
 
         /// <summary>
         /// 短期借款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(622, false)]
         public decimal? ShortLoan { get; set; }
 
         /// <summary>
         /// 交易性金融负债
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(642, false)]
         public decimal? TransactionalFinancialLiabilities { get; set; }
 
         /// <summary>
         /// 应付票据
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(662, false)]
         public decimal? NotesPayable { get; set; }
 
         /// <summary>
         /// 应付账款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(682, false)]
         public decimal? AccountsPayable { get; set; }
 
         /// <summary>
         /// 预收账款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(702, false)]
         public decimal? AccountsAdvance { get; set; }
 
         /// <summary>
         /// 应付利息
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(722, false)]
         public decimal? InterestPayable { get; set; }
 
         /// <summary>
         /// 应付职工薪酬
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(742, false)]
         public decimal? EmployeesSalary { get; set; }
 
         /// <summary>
         /// 应交税费
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(762, false)]
         public decimal? PayTax { get; set; }
 
         /// <summary>
         /// 应付股利
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(782, false)]
         public decimal? PayDividend { get; set; }
 
         /// <summary>
         /// 其他应付款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(802, false)]
         public decimal? OtherPayable { get; set; }
 
         /// <summary>
         /// 一年内到期的非流动负债
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(822, false)]
         public decimal? NonCurrentLiabilitiesInYear { get; set; }
 
         /// <summary>
         /// 其他流动负债
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(842, false)]
         public decimal? OtherCurrentLiabilities { get; set; }
 
         /// <summary>
         /// 流动负债合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(862, true)]
         public decimal? TotalCurrentLiabilities { get; set; }
 
         /// <summary>
         /// 长期借款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(882, false)]
         public decimal? LongLoan { get; set; }
 
         /// <summary>
         /// 应付债券
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(902, false)]
         public decimal? BondPayable { get; set; }
 
         /// <summary>
         /// 长期应付款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(922, false)]
         public decimal? LongAcountsPayable { get; set; }
 
         /// <summary>
         /// 专项应付款
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(942, false)]
         public decimal? SpecialPayment { get; set; }
 
         /// <summary>
         /// 预计负债
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(962, false)]
         public decimal? EstimatedLiabilities { get; set; }
 
         /// <summary>
         /// 递延所得税负债
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(982, false)]
         public decimal? DeferredTaxLiability { get; set; }
 
         /// <summary>
         /// 其他非流动负债
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1002, false)]
         public decimal? OtherNonCurrentLiabilities { get; set; }
 
         /// <summary>
         /// 非流动负债合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1022, true)]
         public decimal TotalNonNurrentLiabilities { get; set; }
 
         /// <summary>
         /// 负债合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1042, true)]
         public decimal TotalLiabilities { get; set; }
 
         /// <summary>
         /// 实收资本(或股本)
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1062, false)]
         public decimal? PaidCapital { get; set; }
 
         /// <summary>
         /// 资本公积
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1082, false)]
         public decimal? CapitalReserve { get; set; }
 
         /// <summary>
         /// 减:库存股
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1102, false)]
         public decimal? Stock { get; set; }
 
         /// <summary>
         /// 盈余公积
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1122, false)]
         public decimal? SurplusReserve { get; set; }
 
         /// <summary>
         /// 未分配利润
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1142, false)]
         public decimal? NoProfit { get; set; }
 
         /// <summary>
         /// 所有者权益合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1162, true)]
         public decimal TotalOwnersEquity { get; set; }
 
         /// <summary>
         /// 负债和所有者权益合计
         /// </summary>
+        [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(1182, true)]
         public decimal TotalLiabilitiesCapital { get; set; }
     }
 }
