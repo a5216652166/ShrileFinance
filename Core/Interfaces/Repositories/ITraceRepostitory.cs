1318df9f6211021a3c85de1a621a6bbae646bad9
七月 <foolishqi@126.com>
2016-12-21 15:31:17 +0800

    注释

--- Core/Interfaces/Repositories/ITraceRepostitory.cs
+++ Core/Interfaces/Repositories/ITraceRepostitory.cs
@@ -6,10 +6,26 @@
 
     public interface ITraceRepostitory : IRepository<Trace>
     {
+        /// <summary>
+        /// 根据跟踪日期和引用标识获取数量
+        /// </summary>
+        /// <param name="traceDate">跟踪日期</param>
+        /// <param name="referenceId">引用标识</param>
+        /// <returns></returns>
         int CountByTraceDateAndReference(DateTime traceDate, Guid referenceId);
 
+        /// <summary>
+        /// 获取最大的序列号跟去跟踪日期
+        /// </summary>
+        /// <param name="traceDate">跟踪日期</param>
+        /// <returns></returns>
         int MaxSerialNumberByTraceDate(DateTime traceDate);
 
+        /// <summary>
+        /// 获取跟踪集合根据跟踪日期
+        /// </summary>
+        /// <param name="traceDate">跟踪日期</param>
+        /// <returns></returns>
         IEnumerable<Trace> GetByTraceDate(DateTime traceDate);
     }
 }
