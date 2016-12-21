55c31588c5c802dd4efe6b6e7b573f923595cbc8
七月 <foolishqi@126.com>
2016-12-21 17:06:41 +0800

    增加还款记录仓储

---
+++ Data/Repositories/PaymentHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Repositories
+{
+    using Core.Entities.Loan;
+    using Core.Interfaces.Repositories;
+
+    public class PaymentHistoryRepository : BaseRepository<PaymentHistory>, IPaymentHistoryRepository
+    {
+        public PaymentHistoryRepository(MyContext context) : base(context)
+        {
+        }
+    }
+}
