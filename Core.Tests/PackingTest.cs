078fe855be24c19be651ffa3e1aa9190cfd11702
王鹏飞 <380807789@qq.com>
2016-12-22 11:09:08 +0800

    5个信息类的构造函数

--- Core.Tests/PackingTest.cs
+++ Core.Tests/PackingTest.cs
@@ -22,18 +22,18 @@ namespace Core.Tests
         {
             var baseParagraph = new BaseParagraph
             {
-                信息记录长度 = 88,
-                信息记录类型 = 04,
+                信息记录长度 = "88",
+                信息记录类型 = "04",
                 借款人名称 = "严冬",
                 贷款卡编号 = "DK01234567898888",
-                报表年份 = 2016,
-                报表类型 = 08,
-                报表类型细分 = 3,
+                报表年份 = "2016",
+                报表类型 = "08",
+                报表类型细分 = "3",
                 审计事务所名称 = "云为科技事务所",
                 审计人员名称 = "王鹏飞",
-                审计时间 = 20161221,
-                信息记录操作类型 = 1,
-                业务发生日期 = 20161220,
+                审计时间 = "20161221",
+                信息记录操作类型 = "1",
+                业务发生日期 = "20161220",
             };
             StringBuilder builder = new StringBuilder();
             baseParagraph.Packaging(builder);
@@ -47,18 +47,18 @@ namespace Core.Tests
         public void ConvertEntity()
         {
             StringBuilder builder = new StringBuilder();
-            Model2Entity.ConvertEntity<BaseParagraph>(builder);
 
-            string filePath = "c:\\Entity.txt";
-            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
-            byte[] data = Encoding.Default.GetBytes(builder.ToString());
+            Type objType = typeof(ProfitsParagraph);
+            string filePath = string.Format("E:\\Configuration\\{0}Configuration.cs", objType.Name);
 
-            fs.Write(data, 0, data.Length);
+            //Model2Entity.ConvertEntity<ProfitsParagraph>(builder);
 
-            fs.Flush();
-            fs.Close();
+            //FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+            //byte[] data = Encoding.Default.GetBytes(builder.ToString());
 
-            System.Diagnostics.Debug.WriteLine(builder.ToString());
+            //fs.Write(data, 0, data.Length);
+            //fs.Flush();
+            //fs.Close();
         }
     }
 }
--- Core/Entities/CreditInvestigation/Segment/BorrowMessage/FinancialAffair/IncomeExpenditureParagraph.cs
+++ Core/Entities/CreditInvestigation/Segment/BorrowMessage/FinancialAffair/IncomeExpenditureParagraph.cs
@@ -1,10 +1,19 @@
-﻿namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffair
+﻿using Core.Entities.Customers.Enterprise;
+
+namespace Core.Entities.CreditInvestigation.Segment.BorrowMessage.FinancialAffair
 {
     /// <summary>
     /// 事业单位收入支出表信息记录
     /// </summary>
     public class IncomeExpenditureParagraph : AbsSegment
     {
+        public IncomeExpenditureParagraph() { }
+
+        public IncomeExpenditureParagraph(InstitutionIncomeExpenditure incomeExpenditure)
+        {
+            AutoMapper.Mapper.Map(incomeExpenditure,this);
+        }
+
         /// <summary>
         /// 报表类型
         /// </summary>
@@ -18,228 +27,228 @@
         /// 财政补助收入
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(2, false)]
-        public decimal? 财政补助收入 { get; set; }
+        public string 财政补助收入 { get; set; }
 
         /// <summary>
         /// 上级补助收入
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(22, false)]
-        public decimal? 上级补助收入 { get; set; }
+        public string 上级补助收入 { get; set; }
 
         /// <summary>
         /// 附属单位缴款
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(42, false)]
-        public decimal? 附属单位缴款 { get; set; }
+        public string 附属单位缴款 { get; set; }
 
         /// <summary>
         /// 事业收入
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(62, false)]
-        public decimal? 事业收入 { get; set; }
+        public string 事业收入 { get; set; }
 
         /// <summary>
         /// 预算外资金收入
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(82, false)]
-        public decimal? 预算外资金收入 { get; set; }
+        public string 预算外资金收入 { get; set; }
 
         /// <summary>
         /// 其他收入
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(102, false)]
-        public decimal? 其他收入 { get; set; }
+        public string 其他收入 { get; set; }
 
         /// <summary>
         /// 事业收入小计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(122, false)]
-        public decimal? 事业收入小计 { get; set; }
+        public string 事业收入小计 { get; set; }
 
         /// <summary>
         /// 经营收入
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(142, false)]
-        public decimal? 经营收入 { get; set; }
+        public string 经营收入 { get; set; }
 
         /// <summary>
         /// 经营收入小计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(162, false)]
-        public decimal? 经营收入小计 { get; set; }
+        public string 经营收入小计 { get; set; }
 
         /// <summary>
         /// 拨入专款
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(182, false)]
-        public decimal? 拨入专款 { get; set; }
+        public string 拨入专款 { get; set; }
 
         /// <summary>
         /// 拨入专款小计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(202, false)]
-        public decimal? 拨入专款小计 { get; set; }
+        public string 拨入专款小计 { get; set; }
 
         /// <summary>
         /// 收入总计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(222, false)]
-        public decimal? 收入总计 { get; set; }
+        public string 收入总计 { get; set; }
 
         /// <summary>
         /// 拨出经费
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(242, false)]
-        public decimal? 拨出经费 { get; set; }
+        public string 拨出经费 { get; set; }
 
         /// <summary>
         /// 上缴上级支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(262, false)]
-        public decimal? 上缴上级支出 { get; set; }
+        public string 上缴上级支出 { get; set; }
 
         /// <summary>
         /// 对附属单位补助
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(282, false)]
-        public decimal? 对附属单位补助 { get; set; }
+        public string 对附属单位补助 { get; set; }
 
         /// <summary>
         /// 事业支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(302, false)]
-        public decimal? 事业支出 { get; set; }
+        public string 事业支出 { get; set; }
 
         /// <summary>
         /// 财政补助支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(322, false)]
-        public decimal? 财政补助支出 { get; set; }
+        public string 财政补助支出 { get; set; }
 
         /// <summary>
         /// 预算外资金支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(342, false)]
-        public decimal? 预算外资金支出 { get; set; }
+        public string 预算外资金支出 { get; set; }
 
         /// <summary>
         /// 销售税金1
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(362, false)]
-        public decimal? 销售税金1 { get; set; }
+        public string 销售税金1 { get; set; }
 
         /// <summary>
         /// 结转自筹基建
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(382, false)]
-        public decimal? 结转自筹基建 { get; set; }
+        public string 结转自筹基建 { get; set; }
 
         /// <summary>
         /// 事业支出小计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(402, false)]
-        public decimal? 事业支出小计 { get; set; }
+        public string 事业支出小计 { get; set; }
 
         /// <summary>
         /// 经营支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(422, false)]
-        public decimal? 经营支出 { get; set; }
+        public string 经营支出 { get; set; }
 
         /// <summary>
         /// 销售税金2
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(442, false)]
-        public decimal? 销售税金2 { get; set; }
+        public string 销售税金2 { get; set; }
 
         /// <summary>
         /// 经营支出小计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(462, false)]
-        public decimal? 经营支出小计 { get; set; }
+        public string 经营支出小计 { get; set; }
 
         /// <summary>
         /// 拨出专款
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(482, false)]
-        public decimal? 拨出专款 { get; set; }
+        public string 拨出专款 { get; set; }
 
         /// <summary>
         /// 专款支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(502, false)]
-        public decimal? 专款支出 { get; set; }
+        public string 专款支出 { get; set; }
 
         /// <summary>
         /// 专款小计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(522, false)]
-        public decimal? 专款小计 { get; set; }
+        public string 专款小计 { get; set; }
 
         /// <summary>
         /// 支出总计
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(542, false)]
-        public decimal? 支出总计 { get; set; }
+        public string 支出总计 { get; set; }
 
         /// <summary>
         /// 事业结余
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(562, true)]
-        public decimal 事业结余 { get; set; }
+        public string 事业结余 { get; set; }
 
         /// <summary>
         /// 正常收入结余
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(582, false)]
-        public decimal? 正常收入结余 { get; set; }
+        public string 正常收入结余 { get; set; }
 
         /// <summary>
         /// 收回以前年度事业支出
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(602, false)]
-        public decimal? 收回以前年度事业支出 { get; set; }
+        public string 收回以前年度事业支出 { get; set; }
 
         /// <summary>
         /// 经营结余
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(622, true)]
-        public decimal 经营结余 { get; set; }
+        public string 经营结余 { get; set; }
 
         /// <summary>
         /// 以前年度经营亏损
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(642, false)]
-        public decimal? 以前年度经营亏损 { get; set; }
+        public string 以前年度经营亏损 { get; set; }
 
         /// <summary>
         /// 结余分配
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(662, false)]
-        public decimal? 结余分配 { get; set; }
+        public string 结余分配 { get; set; }
 
         /// <summary>
         /// 应交所得税
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(682, false)]
-        public decimal? 应交所得税 { get; set; }
+        public string 应交所得税 { get; set; }
 
         /// <summary>
         /// 提取专用基金
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(702, false)]
-        public decimal? 提取专用基金 { get; set; }
+        public string 提取专用基金 { get; set; }
 
         /// <summary>
         /// 转入事业基金
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(722, false)]
-        public decimal? 转入事业基金 { get; set; }
+        public string 转入事业基金 { get; set; }
 
         /// <summary>
         /// 其他结余分配
         /// </summary>
         [MetaCode(20, MetaCodeTypeEnum.AN), SegmentRule(742, false)]
-        public decimal? 其他结余分配 { get; set; }
+        public string 其他结余分配 { get; set; }
     }
 }
