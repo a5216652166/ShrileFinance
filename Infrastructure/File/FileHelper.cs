88fdf3e94ba2ebbbb9603cf62db941e2a84f3e9f
邹强鲁 <Thinkxuhao@outlook.com>
2016-12-23 18:57:44 +0800

    代码规范

--- Infrastructure/File/FileHelper.cs
+++ Infrastructure/File/FileHelper.cs
@@ -1,18 +1,17 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-
-namespace Infrastructure.File
+﻿namespace Infrastructure.File
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+
     public class FileHelper
     {
         public static void Create(string filePath,string content)
         {
-            if (!System.IO.File.Exists(filePath))
+            if (!File.Exists(filePath))
             {
                 FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
                 byte[] data = Encoding.Default.GetBytes(content);
@@ -21,6 +20,18 @@ namespace Infrastructure.File
                 fs.Flush();
                 fs.Close();
             }
+        }
+
+        /// <summary>
+        /// Zip打包
+        /// </summary>
+        /// <param name="dictionary">字典（文件名、流）</param>
+        /// <returns>打包后的流</returns>
+        public static Stream ZipPackaging(IDictionary<string, Stream> dictionary)
+        {
+            Stream stream = new MemoryStream();
+
+            return stream;
         }
     }
 }
