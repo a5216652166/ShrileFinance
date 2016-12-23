77a4e08d5ea6abed1b32923195eb750cfdf8095c
七月 <foolishqi@126.com>
2016-12-23 23:00:46 +0800

    未使用报文增加属性保存.

--- Core/Entities/CreditInvestigation/Datagram/UnusedDatagram.cs
+++ Core/Entities/CreditInvestigation/Datagram/UnusedDatagram.cs
@@ -1,30 +1,24 @@
 ﻿namespace Core.Entities.CreditInvestigation.Datagram
 {
-    using System.Collections.Generic;
-    using Record;
-
     /// <summary>
     /// 未使用信息采集报文
     /// </summary>
     public class UnusedDatagram : AbsDatagram
     {
-        private readonly DatagramTypeEnum type;
-
         public UnusedDatagram(DatagramTypeEnum type)
         {
-            this.type = type;
+            UnusedType = type;
         }
 
         protected UnusedDatagram() : base()
         {
         }
 
+        public DatagramTypeEnum UnusedType { get; private set; }
+
         public override DatagramTypeEnum Type
         {
-            get
-            {
-                return type;
-            }
+            get { return UnusedType; }
         }
     }
 }
