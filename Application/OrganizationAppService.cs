488d9412067733d9977ba57d1acd8079b31eff0c
严冬 <753045147@qq.com>
2016-12-27 18:10:22 +0800

    添加家族信息修改

--- Application/OrganizationAppService.cs
+++ Application/OrganizationAppService.cs
@@ -34,14 +34,35 @@
             customer.AssociatedEnterprises = Mapper.Map<ICollection<AssociatedEnterprise>>(model.AssociatedEnterprises);
             customer.BigEvent = Mapper.Map<ICollection<BigEvent>>(model.BigEvent);
             customer.Litigation = Mapper.Map<ICollection<Litigation>>(model.Litigation);
-            customer.Managers = Mapper.Map<ICollection<Manager>>(model.Managers);
-            customer.Shareholders = Mapper.Map<ICollection<Stockholder>>(model.Shareholders);
+
+
+            new UpdateBind().Bind(customer.Managers, model.Managers);
+
+            model.Managers.ToList().ForEach(m =>
+            {
+                var familyMembersEntity = customer.Managers.ToList().Find(c => c.Id == m.Id && c.Id != Guid.Empty);
+                if (familyMembersEntity != null)
+                {
+                    new UpdateBind().Bind(familyMembersEntity.FamilyMembers, m.FamilyMembers);
+                }
+            });
+            new UpdateBind().Bind(customer.Shareholders, model.Shareholders);
+
+            model.Shareholders.ToList().ForEach(m =>
+            {
+                var shareholders = customer.Shareholders.ToList().Find(c => c.Id == m.Id && c.Id != Guid.Empty);
+                if (shareholders != null)
+                {
+                    new UpdateBind().Bind(shareholders.FamilyMembers, m.FamilyMembers);
+                }
+            });
 
             if (model.FinancialAffairs != null)
             {
                 customer.FinancialAffairs = new FinancialAffairs()
                 {
                     Id = Guid.Empty,
+                    AuditorDate = model.FinancialAffairs.AuditorDate,
                     Year = model.FinancialAffairs.Year,
                     TypeSubdivision = model.FinancialAffairs.TypeSubdivision,
                     AuditFirm = model.FinancialAffairs.AuditFirm,
@@ -56,7 +77,6 @@
             }
 
             repository.Create(customer);
-
             repository.Commit();
 
             // 报文追踪
@@ -82,14 +102,6 @@
             new UpdateBind().Bind(customer.BigEvent, model.BigEvent);
             new UpdateBind().Bind(customer.Litigation, model.Litigation);
             new UpdateBind().Bind(customer.Managers, model.Managers);
-
-            //model.Managers.ToList().ForEach(m=> {
-            //    // 获取家族成员的id集合
-            //    var ids = (from t in m.FamilyMembers select t.Id.Value).ToList();
-
-            //    // 移除多余项
-            //    customer.Managers.ToList().Find(c => c.Id == m.Id).FamilyMembers.ToList().RemoveAll(f=>!ids.Contains(f.Id));
-            //});
 
             model.Managers.ToList().ForEach(m =>
             {
