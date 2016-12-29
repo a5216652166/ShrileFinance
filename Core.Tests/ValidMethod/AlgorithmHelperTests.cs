b2cd3b2eb722890aa41b3880ab58f60d52e6de05
邹强鲁 <Thinkxuhao@outlook.com>
2016-12-29 16:35:03 +0800

    新增单元测试

---
+++ Core.Tests/ValidMethod/AlgorithmHelperTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Infrastructure.ValidMethod.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Infrastructure.ValidMethod;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass()]
+    public class AlgorithmHelperTests
+    {
+        [TestMethod()]
+        public void LulnMethodTest()
+        {
+            var luln = "79927398713";
+
+            var result = luln.LulnMethod();
+
+            Assert.IsTrue(result.Key);
+        }
+    }
+}
