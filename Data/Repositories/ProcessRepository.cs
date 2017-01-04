3cee4f828bbdcd5d486598ab1af856dabd785a82
邹强鲁 <Thinkxuhao@outlook.com>
2017-01-04 11:10:43 +0800

    重命名FlowRepository为ProcessRepository

---
+++ Data/Repositories/ProcessRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Data.Repositories
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Core.Entities.Flow;
+    using Core.Interfaces.Repositories;
+
+    public class ProcessRepository : BaseRepository<Flow>, IFlowRepository
+    {
+        public ProcessRepository(MyContext context) : base(context)
+        {
+        }
+
+        public override IQueryable<Flow> GetAll()
+        {
+            return base.GetAll();
+        }
+
+        public override IQueryable<Flow> GetAll(Expression<Func<Flow, bool>> predicate)
+        {
+            return base.GetAll(predicate);
+        }
+    }
+}
