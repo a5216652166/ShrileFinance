6bb2f8a765cb84539938d3cfe5a3c67d47afa551
邹强鲁 <Thinkxuhao@outlook.com>
2017-01-10 18:23:26 +0800

    代办已办列表模糊查询，及代码优化

--- data/Repositories/InstanceRepository.cs
+++ data/Repositories/InstanceRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace Data.Repositories
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using Core.Entities;
     using Core.Entities.Flow;
@@ -24,34 +25,19 @@
                 && (m.CurrentUserId == null || m.CurrentUserId == currentUser.Id));
 
             // 标题模糊搜索
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                instances = instances.Where(m => m.Title.Contains(searchString));
-            }
+            instances = FiterForTitle(instances, searchString);
 
             // 流程筛选
-            if (flowId.HasValue)
-            {
-                instances = instances.Where(m => m.FlowId == flowId);
-            }
+            instances = FiterForFlowId(instances, flowId);
 
             // 节点筛选
-            if (currentNodeId.HasValue)
-            {
-                instances = instances.Where(m => m.CurrentNodeId == currentNodeId);
-            }
+            instances = FiterForNodeId(instances, currentNodeId);
 
             // 开始时间筛选
-            if (beginTime.HasValue)
-            {
-                instances = instances.Where(m => m.StartTime >= beginTime);
-            }
+            instances = FiterForBeginTime(instances, beginTime);
 
             // 结束时间筛选
-            if (endTime.HasValue)
-            {
-                instances = instances.Where(m => m.StartTime < endTime);
-            }
+            instances = FiterForEndTime(instances, endTime);
 
             // 排序(发起时间降序)
             instances = instances.OrderByDescending(m => m.StartTime);
@@ -67,46 +53,75 @@
                 m.Logs.Any(n => n.ProcessUserId == currentUser.Id));
 
             // 标题模糊搜索
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                instances = instances.Where(m => m.Title.Contains(searchString));
-            }
+            instances = FiterForTitle(instances, searchString);
 
             // 流程筛选
-            if (flowId.HasValue)
-            {
-                instances = instances.Where(m => m.FlowId == flowId);
-            }
+            instances = FiterForFlowId(instances, flowId);
 
             // 节点筛选
-            if (currentNodeId.HasValue)
-            {
-                instances = instances.Where(m => m.CurrentNodeId == currentNodeId);
-            }
+            instances = FiterForNodeId(instances, currentNodeId);
 
             // 开始时间筛选
-            if (beginTime.HasValue)
-            {
-                instances = instances.Where(m => m.StartTime >= beginTime);
-            }
+            instances = FiterForBeginTime(instances, beginTime);
 
             // 结束时间筛选
-            if (endTime.HasValue)
-            {
-                instances = instances.Where(m => m.StartTime < endTime);
-            }
+            instances = FiterForEndTime(instances, endTime);
 
             // 状态筛选
-            if (status.HasValue)
-            {
-                instances = instances.Where(m => m.Status == status);
-            }
+            instances = FiterForStatus(instances, status);
 
             // 排序(处理时间降序)
             instances = instances.OrderByDescending(m => m.ProcessTime);
 
             // 分页查询
             return instances.ToPagedList(page, size);
+        }
+
+        private IQueryable<Instance> FiterForTitle(IQueryable<Instance> refSource, string searchString)
+        {
+            searchString = searchString?.Trim();
+
+            if (string.IsNullOrEmpty(searchString) || refSource == null)
+            {
+                return refSource;
+            }
+
+            var outSource = new List<Instance>();
+
+            foreach (var item in refSource)
+            {
+                if (item.Title != null && item.Title.Contains(searchString))
+                {
+                    outSource.Add(item);
+                }
+            }
+
+            return outSource.AsQueryable();
+        }
+
+        private IQueryable<Instance> FiterForFlowId(IQueryable<Instance> refSource, Guid? flowId) =>
+            IsNull(refSource, flowId) ? refSource : refSource.Where(m => m.FlowId == flowId);
+
+        private IQueryable<Instance> FiterForNodeId(IQueryable<Instance> refSource, Guid? nodeId) =>
+            IsNull(refSource, nodeId) ? refSource : refSource.Where(m => m.CurrentNodeId == nodeId);
+
+        private IQueryable<Instance> FiterForBeginTime(IQueryable<Instance> refSource, DateTime? beginTime) =>
+            IsNull(refSource, beginTime) ? refSource : refSource.Where(m => m.StartTime == beginTime);
+
+        private IQueryable<Instance> FiterForEndTime(IQueryable<Instance> refSource, DateTime? endTime) =>
+            IsNull(refSource, endTime) ? refSource : refSource.Where(m => m.EndTime == endTime);
+
+        private IQueryable<Instance> FiterForStatus(IQueryable<Instance> refSource, InstanceStatusEnum? status) =>
+            IsNull(refSource, status) ? refSource : refSource.Where(m => m.Status == status);
+
+        private bool IsNull<T>(IQueryable<Instance> refSource, T obj)
+        {
+            if (refSource != null && obj != null)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
