cfe86b214840c23b28904146d673fd280d2247fd
严冬 <753045147@qq.com>
2017-02-13 10:28:29 +0800

    修改序列号BUG

--- Data/Repositories/DatagramFileRepository.cs
+++ Data/Repositories/DatagramFileRepository.cs
@@ -18,20 +18,26 @@
 
             // 获取指定时间、指定类型的所有报文文件集合
             var files = GetAll(m => m.DateCreated.Date == datagramFile.DateCreated.Date && m.Type == datagramFile.Type);
-
-            // 从files查找指定TraceId的报文文件集合
-            var file = files.Where(m => m.TraceId == datagramFile.TraceId);
-
-            // 若数据库已经记录该数据，则使用数据库中记录的序列号；否则，分配新序列号
-            if (file.Count() == 1)
+            if (files.Count() == 0)
             {
-                serialNumber = file.First().SerialNumber;
+                serialNumber = "0001";
             }
             else
             {
-                var filesMaxSerialNumber = files.Max(m => Convert.ToInt32(m.SerialNumber));
+                // 从files查找指定TraceId的报文文件集合
+                var file = files.Where(m => m.TraceId == datagramFile.TraceId);
 
-                serialNumber = (Math.Max(files.Count(), filesMaxSerialNumber) + 1).ToString("D4");
+                // 若数据库已经记录该数据，则使用数据库中记录的序列号；否则，分配新序列号
+                if (file.Count() == 1)
+                {
+                    serialNumber = file.First().SerialNumber;
+                }
+                else
+                {
+                    var filesMaxSerialNumber = files.Max(m => Convert.ToInt32(m.SerialNumber));
+
+                    serialNumber = (Math.Max(files.Count(), filesMaxSerialNumber) + 1).ToString("D4");
+                }
             }
 
             return serialNumber;
