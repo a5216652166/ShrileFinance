a7860ad96a3bacc913942fd943bc7ca13e68bb5c
严冬 <753045147@qq.com>
2017-02-23 13:47:20 +0800

    添加合同定时服务

--- Application/TimeAppService.cs
+++ Application/TimeAppService.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using Core.Entities.CreditInvestigation;
     using Core.Entities.Loan;
     using Core.Interfaces.Repositories;
     using Data;
@@ -11,27 +12,43 @@
     {
         private readonly ICreditContractRepository contractRepository;
         private readonly ILoanRepository loanRepostiory;
+        private readonly ITraceRepostitory traceRepostitory;
 
         public TimeAppService()
         {
             MyContext context = new MyContext();
             this.contractRepository = new CreditContractRepository(context); ;
             loanRepostiory = new LoanRepository(context);
+            traceRepostitory = new TraceRepository(context);
         }
 
         public void TimeService()
         {
-            var contract = contractRepository.GetAll(m => m.EffectiveStatus == Core.Entities.Loan.CreditContractStatusEnum.生效);
+            ContractEnd();
+        }
 
-            //var loans = loanRepostiory.GetAll(m => m.Status == LoanStatusEnum.正常);
-            if (contract.Count()>0)
+        /// <summary>
+        /// 合同到期定时终止
+        /// </summary>
+        private void ContractEnd()
+        {
+            var aaa = contractRepository.GetAll();
+            var contract = contractRepository.GetAll(m => m.EffectiveStatus == CreditContractStatusEnum.生效);
+
+            if (contract.Count() > 0)
             {
                 foreach (var item in contract)
                 {
-                    if(item.ExpirationDate <= DateTime.Now)
+                    if (item.ExpirationDate <= DateTime.Now)
                     {
                         item.EffectiveStatus = CreditContractStatusEnum.失效;
                         contractRepository.Modify(item);
+                        var count = traceRepostitory.CountByDateCreatedAndReference(DateTime.Now.Date, item.Id, TraceTypeEnum.签订授信合同);
+                        if (count == 0)
+                        {
+                            var trace = new Trace(item.Id, TraceTypeEnum.签订授信合同, item.EffectiveDate, item.Organization.Property.InstitutionChName, "贷款合同：" + item.CreditContractCode + "终止");
+                            traceRepostitory.Create(trace);
+                        }
                     }
                 }
             }
