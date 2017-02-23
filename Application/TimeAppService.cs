87491a5e7455c40b8cd189852742ccf6012c84ca
严冬 <753045147@qq.com>
2017-02-23 10:02:47 +0800

    添加定时服务

---
+++ Application/TimeAppService.cs
@@ -0,0 +1,41 @@
+﻿namespace Application
+{
+    using System;
+    using System.Linq;
+    using Core.Entities.Loan;
+    using Core.Interfaces.Repositories;
+    using Data;
+    using Data.Repositories;
+
+    public class TimeAppService
+    {
+        private readonly ICreditContractRepository contractRepository;
+        private readonly ILoanRepository loanRepostiory;
+
+        public TimeAppService()
+        {
+            MyContext context = new MyContext();
+            this.contractRepository = new CreditContractRepository(context); ;
+            loanRepostiory = new LoanRepository(context);
+        }
+
+        public void TimeService()
+        {
+            var contract = contractRepository.GetAll(m => m.EffectiveStatus == Core.Entities.Loan.CreditContractStatusEnum.生效);
+
+            //var loans = loanRepostiory.GetAll(m => m.Status == LoanStatusEnum.正常);
+            if (contract.Count()>0)
+            {
+                foreach (var item in contract)
+                {
+                    if(item.ExpirationDate <= DateTime.Now)
+                    {
+                        item.EffectiveStatus = CreditContractStatusEnum.失效;
+                        contractRepository.Modify(item);
+                    }
+                }
+            }
+            contractRepository.Commit();
+        }
+    }
+}
---
+++ TimeService/Service1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+using Application;
+
+namespace TimeService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private TimeAppService timeAppService;
+
+        // 计时器
+        Timer timer;
+        public Service1()
+        {
+            InitializeComponent();
+            timeAppService = new TimeAppService();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer(50000);
+            timer.Elapsed += new ElapsedEventHandler(Time_StartElapsed);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Elapsed += new ElapsedEventHandler(Timer_StopElapsed);
+
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        void Timer_StopElapsed(object sender, ElapsedEventArgs e)
+        {
+            //string filePath = AppDomain.CurrentDomain.BaseDirectory + "test.txt";
+            string filePath = "D:/baa.txt";
+
+            StreamWriter sw = null;
+            if (!File.Exists(filePath))
+            {
+                sw = File.CreateText(filePath);
+            }
+            else
+            {
+                sw = File.AppendText(filePath);
+            }
+            sw.Write("访问时间：" + DateTime.Now.ToString() + Environment.NewLine);
+            sw.Close();
+        }
+        public void Time_StartElapsed(object sender, ElapsedEventArgs e)
+        {
+            timeAppService.TimeService();
+        }
+    }
+}
