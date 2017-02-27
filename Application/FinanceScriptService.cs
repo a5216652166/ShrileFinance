798253127b4e124eff0f6493ab579e6c5f790f59
邹强鲁 <Thinkxuhao@outlook.com>
2017-02-27 10:19:26 +0800

    还款流程中，还款时间改为实际还款时间

--- Application/FinanceScriptService.cs
+++ Application/FinanceScriptService.cs
@@ -295,7 +295,7 @@
             // 设置流程实例关联的业务标识
             Instance.RootKey = payment.LoanId;
             var loan = loanAppService.Get(payment.LoanId);
-            Instance.Title = $"{"借据编号：" + loan.ContractNumber + " 还款时间:" + DateTime.Now.ToString("yyyy-MM-dd")}";
+            Instance.Title = $"{"借据编号：" + loan.ContractNumber + " 还款时间:" + payment.Payments.Last().ActualDatePayment.ToString("yyyy-MM-dd")}";
         }
 
         /// <summary>
