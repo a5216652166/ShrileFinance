40746f3b76d0bbd841332fb3e3116b5d84ea8738
邹强鲁 <Thinkxuhao@outlook.com>
2017-02-27 09:48:53 +0800

    有权审核人 已办列表查看功能修复

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -408,6 +408,7 @@
                 new FormRole { RoleId = "C642BEE1-05A4-E611-80C5-507B9DE4A488", FormId = new Guid("61DC5FCF-18A4-E611-80C5-507B9DE4A488") },
                 new FormRole { RoleId = "C642BEE1-05A4-E611-80C5-507B9DE4A488", FormId = new Guid("62DC5FCF-18A4-E611-80C5-507B9DE4A488") },
                 new FormRole { RoleId = "C642BEE1-05A4-E611-80C5-507B9DE4A488", FormId = new Guid("63DC5FCF-18A4-E611-80C5-507B9DE4A488") },
+                new FormRole { RoleId = "C642BEE1-05A4-E611-80C5-507B9DE4A488", FormId = new Guid("FFFFFFFF-18A4-E611-80C5-507B9DE4A488") },
 
                 // 出纳
                 new FormRole { RoleId = "C542BEE1-05A4-E611-80C5-507B9DE4A488", FormId = new Guid("62DC5FCF-18A4-E611-80C5-507B9DE4A488") },
